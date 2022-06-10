4fab9df244d612c8565184fab243f5c3fc2094bc
Shawn Huang Fernandes <55112397+shawnhuangfernandes@users.noreply.github.com>
2022-06-10 16:05:08 -0700

    Particle Water Splash

--- Assets/SkipVisuals.cs
+++ Assets/SkipVisuals.cs
@@ -1,18 +1,32 @@
+// Author:  Shawn Huang Fernandes
+// Date:    06/10/22
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Component that handles visuals of rock skipping
+/// This includes the water movement, spawning VFX
+/// </summary>
+
 public class SkipVisuals : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Water Materials")]
+    [SerializeField] private Material WaterMaterial;
+    [SerializeField] private Material SandMaterial;
 
-    // Update is called once per frame
-    void Update()
+    [Header("Splash Effects")]
+    [SerializeField] private GameObject SplashParticle;
+
+    [Header("Speed")]
+    public float Speed = 0F;
+
+    public void SpawnSplash()
     {
-        
+        Debug.Log("Doing this!");
+        Transform rockXFRM = FindObjectOfType<RockController>().transform;
+        Vector3 spawnPosition = new Vector3(rockXFRM.position.x, 0F, rockXFRM.position.y);
+        ParticleSystem splash = Instantiate(SplashParticle, spawnPosition, Quaternion.identity).GetComponent<ParticleSystem>();
     }
 }
