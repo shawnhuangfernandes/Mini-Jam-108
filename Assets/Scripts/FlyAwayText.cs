6c06705b7a812aa62d206f5d29209f1db46fd63c
Shawn Huang Fernandes <55112397+shawnhuangfernandes@users.noreply.github.com>
2022-06-11 23:36:44 -0700

    Added Flyaway Text

---
+++ Assets/Scripts/FlyAwayText.cs
@@ -0,0 +1,50 @@
+// Author:  Shawn Huang Fernandes
+// Date:    06/11/22
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+/// <summary>
+/// Text that "flies" away on spawning
+/// </summary>
+
+    [RequireComponent(typeof (TextMeshPro))]
+public class FlyAwayText : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The displacement of the text after it spawns")]
+    private Vector3 LocalDisplacement;
+
+    [SerializeField]
+    [Tooltip("The duration of the flight")]
+    private float DisplacementDuration;
+
+    [SerializeField]
+    [Tooltip("The duration of the text FadeIn")]
+    private float FadeInDuration;
+
+    private TextMeshPro _tmp;
+    private TextMeshPro tmp
+    {
+        get
+        {
+            if (_tmp == null)
+                _tmp = GetComponent<TextMeshPro>();
+
+            return _tmp;
+        }
+    }
+
+    public void Start()
+    {
+        _tmp.DOFade(1, FadeInDuration)
+            .SetEase(Ease.InQuad);
+
+        transform.DOMove(transform.position + LocalDisplacement, DisplacementDuration)
+            .SetEase(Ease.InBounce)
+            .OnComplete(() => Destroy(gameObject));
+    }
+}
